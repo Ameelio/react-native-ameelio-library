e1df48f1a6fbb65dc9f5e8ac9ec41d01564d4b6a
Jordan Witte <wittejm@gmail.com>
2021-10-05 11:06:25 -0700

    Add rows for transparent colors to storybook (#45)
    Add rows for transparent colors to storybook.
    
    * These already get displayed in the palate, but are added again with a blue background for clarity (reflecting Figma).
    
    * Not included is the brand Gradient Red/Blue. `View` doesn't support the `background` style property (only `backgroundColor`), so figuring out where to express this gradient in the components library and storybook is WIP.

--- storybook/stories/Brand/Colors.stories.tsx
+++ storybook/stories/Brand/Colors.stories.tsx
@@ -5,6 +5,7 @@ import CenterView from "../../helpers/CenterView.react";
 import { ScrollView, View } from "react-native";
 
 const colorsArray = Object.values(Colors);
+const BACKGROUND_COLOR = "rgba(200, 226, 246, 1)";
 
 storiesOf("Brand/Colors", module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
@@ -20,6 +21,31 @@ storiesOf("Brand/Colors", module)
               />
             );
           })}
+        </View>
+        <View style={{ flexDirection: "row", flexWrap: "wrap", backgroundColor: BACKGROUND_COLOR }}>
+          {[Colors.BLACK, Colors.BLACK_65, Colors.BLACK_45, Colors.BLACK_25, Colors.BLACK_06].map((color, index) => {
+            return (
+              <View
+                style={{ height: 60, margin: 8, width: "14.28%", backgroundColor: color }}
+                key={index.toString()}
+              />
+            );
+          })}
+        </View>
+        <View style={{ flexDirection: "row", flexWrap: "wrap", backgroundColor: BACKGROUND_COLOR }}>
+          {[Colors.WHITE, Colors.WHITE_65, Colors.WHITE_45, Colors.WHITE_25].map((color, index) => {
+            return (
+              <View
+                style={{ height: 60, margin: 8, width: "14.28%", backgroundColor: color }}
+                key={index.toString()}
+              />
+            );
+          })}
+        </View>
+        <View style={{ flexDirection: "row", flexWrap: "wrap", backgroundColor: BACKGROUND_COLOR }}>
+          <View
+            style={{ height: 60, margin: 8, width: "14.28%", backgroundColor: Colors.WHITE_BACKGROUND }}
+          />
         </View>
       </ScrollView>
     );
