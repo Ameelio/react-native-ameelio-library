9fb387975b9cd2366d2c90fb96a6186a0e7021be
Jordan Witte <wittejm@gmail.com>
2021-10-15 08:40:51 -0700

    Remove unused logos from component library (#68)
    * Also update AmeelioPrimary svg with fix.

--- storybook/stories/Brand/Logo.stories.tsx
+++ storybook/stories/Brand/Logo.stories.tsx
@@ -5,9 +5,6 @@ import CenterView from "../../helpers/CenterView.react";
 
 storiesOf("Brand/Logo", module)
   .addDecorator((getStory) => <CenterView>{getStory()}</CenterView>)
-  .add("Red", () => <Logo type="red" width={200} />)
-  .add("Blue", () => <Logo type="blue" width={200} />)
-  .add("Ameelio", () => <Logo type="ameelio" width={200} />)
   .add("Ameelio Primary", () => <Logo type="ameelio-primary" width={200} />)
   .add("Ameelio Monocolor", () => <Logo type="ameelio-monocolor" width={200} />)
   .add("Ameelio Plus", () => <Logo type="ameelio-plus" width={200} />)
