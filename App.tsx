9e3553b43257f1412a3ce9b2e4ce308ffedb6cd5
gabrielsaruhashi <gabriel.saruhashi@gmail.com>
2022-02-17 10:59:37 -0300

    comment out inter

--- App.tsx
+++ App.tsx
@@ -3,43 +3,52 @@ const StorybookUIRoot = require("./storybook").default;
 import { StatusBar, View } from "react-native";
 
 import AppLoading from "expo-app-loading";
-import {
-  useFonts,
-  Inter_100Thin,
-  Inter_200ExtraLight,
-  Inter_300Light,
-  Inter_400Regular,
-  Inter_500Medium,
-  Inter_600SemiBold,
-  Inter_700Bold,
-  Inter_800ExtraBold,
-  Inter_900Black,
-} from "@expo-google-fonts/inter";
+// import {
+//   useFonts,
+//   Inter_100Thin,
+//   Inter_200ExtraLight,
+//   Inter_300Light,
+//   Inter_400Regular,
+//   Inter_500Medium,
+//   Inter_600SemiBold,
+//   Inter_700Bold,
+//   Inter_800ExtraBold,
+//   Inter_900Black,
+// } from "@expo-google-fonts/inter";
 
 export default () => {
-  let [fontsLoaded] = useFonts({
-    Inter_100Thin,
-    Inter_200ExtraLight,
-    Inter_300Light,
-    Inter_400Regular,
-    Inter_500Medium,
-    Inter_600SemiBold,
-    Inter_700Bold,
-    Inter_800ExtraBold,
-    Inter_900Black,
-  });
+  // let [fontsLoaded] = useFonts({
+  //   Inter_100Thin,
+  //   Inter_200ExtraLight,
+  //   Inter_300Light,
+  //   Inter_400Regular,
+  //   Inter_500Medium,
+  //   Inter_600SemiBold,
+  //   Inter_700Bold,
+  //   Inter_800ExtraBold,
+  //   Inter_900Black,
+  // });
 
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  } else {
-    return (
+  return (
+    <View style={{ flex: 1 }}>
+      <StatusBar barStyle="dark-content" />
       <View style={{ flex: 1 }}>
-        <StatusBar barStyle="dark-content" />
-        <View style={{ flex: 1 }}>
-          <StorybookUIRoot />
-        </View>
-        <View style={{ height: 30 }} />
+        <StorybookUIRoot />
       </View>
-    );
-  }
+      <View style={{ height: 30 }} />
+    </View>
+  );
+  // if (!fontsLoaded) {
+  //   return <AppLoading />;
+  // } else {
+  //   return (
+  //     <View style={{ flex: 1 }}>
+  //       <StatusBar barStyle="dark-content" />
+  //       <View style={{ flex: 1 }}>
+  //         <StorybookUIRoot />
+  //       </View>
+  //       <View style={{ height: 30 }} />
+  //     </View>
+  //   );
+  // }
 };
